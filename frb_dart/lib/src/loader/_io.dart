833ec7b134ace6fb66b448acfcac41e3852cc673
p_jwcao <p_jwcao@tencent.com>
2025-06-23 15:12:45 +0800

    feat: ohos support

--- frb_dart/lib/src/loader/_io.dart
+++ frb_dart/lib/src/loader/_io.dart
@@ -52,7 +52,7 @@ ExternalLibrary loadExternalLibraryRaw({
     return ExternalLibrary.open(filePath);
   }
 
-  if (Platform.isAndroid) {
+  if (Platform.isAndroid || Platform.operatingSystem == "ohos") {
     return ExternalLibrary.open('lib$stem.so');
   }
 
